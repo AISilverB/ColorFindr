dd9a5e4f8627350b4d0abf43967086a053f8c7f1
Dhinedhineee <dhinedhineee@gmail.com>
2018-11-23 16:17:02 +0800

    MainActivity, GameActivity

--- app/src/main/java/cs173/colorfindr/MainActivity.java
+++ app/src/main/java/cs173/colorfindr/MainActivity.java
@@ -1,13 +1,28 @@
 package cs173.colorfindr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playGame;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+0
+//        playGame = (Button) findViewById(R.id.playbutton);
+//
+//        playGame.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent mainIntent = new Intent(MainActivity.this, GameActivity.class);
+//                startActivity(mainIntent);
+//
+//            }
+//        });
     }
 }

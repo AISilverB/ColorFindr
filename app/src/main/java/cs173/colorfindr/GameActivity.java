bc30cc262af0f0346784a75a2c2b8d8f449321d2
Dhinedhineee <dhinedhineee@gmail.com>
2018-11-28 01:39:08 +0800

    HS + 3 lives + 50 level

--- app/src/main/java/cs173/colorfindr/GameActivity.java
+++ app/src/main/java/cs173/colorfindr/GameActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Color;
 import android.graphics.ImageFormat;
 import android.graphics.Matrix;
@@ -35,11 +36,22 @@ import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -78,11 +90,12 @@ public class GameActivity extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
 
-    CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+    private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
             cameraDevice = camera;
             createCameraPreview();
+            Log.d(TAG, "whut");
         }
 
         @Override
@@ -97,16 +110,23 @@ public class GameActivity extends AppCompatActivity {
     };
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "ONCREATE");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        init();
-
         textureView = findViewById(R.id.textureView);
-        //From Java 1.4 , you can use keyword 'assert' to check expression true or false
-        assert textureView != null;
-        textureView.setSurfaceTextureListener(textureListener);
+
+
+        if (textureView != null){
+            textureView.setSurfaceTextureListener(textureListener);
+            Log.d(TAG, "why not null: ");
+        } else {
+            Log.d(TAG, "why null: ");
+
+        }
+
         ImageButton btnCapture = findViewById(R.id.btnCapture);
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -114,13 +134,42 @@ public class GameActivity extends AppCompatActivity {
                 takePicture();
             }
         });
+
+
+        init();
     }
 
-    private void init(){
-        level = 40;
+    private void init() {
+        level = 50;
         currscore = 0;
-        currlives = 10;
-        currhighscore = 0;
+        currlives = 3;
+
+
+        String line = "";
+        FileInputStream fis = null;
+        try {
+            File file = new File(this.getFilesDir(),"cfg");
+            File gpxfile = new File(file, "gameconfig.txt");
+            FileReader reader = new FileReader(gpxfile);
+            if(file.exists())   Log.d(TAG, "MERONNNNNN NA");
+            fis = new FileInputStream(gpxfile);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+//            while ((line = bufferedReader.readLine()) != null) {
+//                Log.d(TAG, "linemo: " + line);
+//            }
+            line = bufferedReader.readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            currhighscore = Integer.parseInt(line);
+            Log.d(TAG, "save naman eh :(");
+        } catch(Exception e) {
+            e.printStackTrace();
+            currhighscore = 0;
+        }
         currctr = -1;
         retrieveColorList();
         change = true;
@@ -135,7 +184,6 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void takePicture() {
-
         if(cameraDevice == null)
             return;
         CameraManager manager = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
@@ -214,7 +262,7 @@ public class GameActivity extends AppCompatActivity {
                 public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                     try{
                         cameraCaptureSession.capture(captureBuilder.build(),captureListener,mBackgroundHandler);
-                    } catch (CameraAccessException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
@@ -238,7 +286,7 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void run() {
                 while(true) {
-                    Log.d(TAG, "I AM A THREAD: " + change);
+//                    Log.d(TAG, "I AM A THREAD: " + change);
                     if (change) {
                         runOnUiThread(new Runnable() {
                             @Override
@@ -350,10 +398,33 @@ public class GameActivity extends AppCompatActivity {
             }
         } else {
             currlives--;
+            if (currlives == 0){
+                savehighscore();
+            }
         }
 
         change = true;
     }
+
+    private void savehighscore() {
+        File file = new File(this.getFilesDir(),"cfg");
+        if(!file.exists()){
+            file.mkdir();
+        }
+
+        try{
+            File gpxfile = new File(file, "gameconfig.txt");
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.write(Integer.toString(currhighscore));
+            writer.flush();
+            writer.close();
+            Log.d(TAG, "FILE SAVEDDDD");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finish();
+    }
+
 
     private void logme(String y, int x){
         Log.d(TAG, y +": " + Integer.toString(x));
@@ -363,17 +434,26 @@ public class GameActivity extends AppCompatActivity {
 
     private void createCameraPreview() {
         try{
+            Log.d(TAG, "CREATECAMERAPREVIEW");
             SurfaceTexture texture = textureView.getSurfaceTexture();
             assert  texture != null;
+            Log.d(TAG, "Texture is not null");
+
             texture.setDefaultBufferSize(imageDimension.getWidth(),imageDimension.getHeight());
             Surface surface = new Surface(texture);
             captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
             captureRequestBuilder.addTarget(surface);
+
+            Log.d(TAG, "Target added");
+
             cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
-                    if(cameraDevice == null)
+                    if(cameraDevice == null) {
+                        Log.d(TAG, "Camera device is null");
                         return;
+                    }
                     cameraCaptureSessions = cameraCaptureSession;
                     updatePreview();
                 }
@@ -383,18 +463,20 @@ public class GameActivity extends AppCompatActivity {
                     Toast.makeText(GameActivity.this, "Changed", Toast.LENGTH_SHORT).show();
                 }
             },null);
-        } catch (CameraAccessException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void updatePreview() {
+        Log.d(TAG, "UPDATEPREVIEW");
+
         if(cameraDevice == null)
             Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
         captureRequestBuilder.set(CaptureRequest.CONTROL_MODE,CaptureRequest.CONTROL_MODE_AUTO);
         try{
             cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(),null,mBackgroundHandler);
-        } catch (CameraAccessException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -407,12 +489,16 @@ public class GameActivity extends AppCompatActivity {
             if (manager != null) {
                 cameraId = manager.getCameraIdList()[0];
             }
+            Log.d(TAG, "MANAGER NOT NULL");
             CameraCharacteristics characteristics = null;
             if (cameraId != null) {
                 characteristics = manager.getCameraCharacteristics(cameraId);
             }
+            Log.d(TAG, "CAMERA ID NOT NULL");
             StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
             assert map != null;
+
+            Log.d(TAG, "MAP NOT NULL");
             imageDimension = map.getOutputSizes(SurfaceTexture.class)[0];
             //Check realtime permission if run higher API 23
             if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
@@ -421,19 +507,26 @@ public class GameActivity extends AppCompatActivity {
                         Manifest.permission.CAMERA,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE
                 },REQUEST_CAMERA_PERMISSION);
+
+                Log.d(TAG, "DONE X PERMISSIONS");
                 return;
             }
             manager.openCamera(cameraId,stateCallback,null);
 
+            Log.d(TAG, "MANAGER OPEN CAMERA ");
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
-    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+
+
+    private TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+            Log.d(TAG, "Available surface");
             openCamera();
+            Log.d(TAG, "done camera");
         }
 
         @Override
@@ -443,6 +536,7 @@ public class GameActivity extends AppCompatActivity {
 
         @Override
         public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            Log.d(TAG, "Surface Destroyed");
             return false;
         }
 
@@ -450,6 +544,7 @@ public class GameActivity extends AppCompatActivity {
         public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
 
         }
+
     };
 
     @Override
@@ -468,25 +563,26 @@ public class GameActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         startBackgroundThread();
-        if(textureView.isAvailable())
-            openCamera();
-        else
-            textureView.setSurfaceTextureListener(textureListener);
+        Log.d(TAG, "resuming");
     }
 
     @Override
     protected void onPause() {
-        stopBackgroundThread();
         super.onPause();
+        if (cameraDevice != null) {
+            cameraDevice.close();
+        }
+        stopBackgroundThread();
     }
 
     private void stopBackgroundThread() {
-        mBackgroundThread.quitSafely();
         try{
+            mBackgroundThread.quitSafely();
             mBackgroundThread.join();
             mBackgroundThread= null;
             mBackgroundHandler = null;
-        } catch (InterruptedException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -495,5 +591,7 @@ public class GameActivity extends AppCompatActivity {
         mBackgroundThread = new HandlerThread("Camera Background");
         mBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+        Log.d(TAG, "THREADS RUNNING");
     }
+
 }
